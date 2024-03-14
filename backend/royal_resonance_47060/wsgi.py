79275a9f0f4b0eedde6fccf1a952004c98abcc05
Crowdbotics <team@crowdbotics.com>
2024-03-14 17:59:28 +0000

    Initial commit

---
+++ backend/royal_resonance_47060/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for royal_resonance_47060 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "royal_resonance_47060.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "royal-resonance-47060",
+  url: "https://royal-resonance-47060.botics.co",
+  api: "https://royal-resonance-47060.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
